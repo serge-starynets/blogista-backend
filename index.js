287fd0d939c0a041289924268785a82fcb3d1207
serge-starynets <sergii.starynets@gmail.com>
2025-06-17 13:23:10 +0800

    add prod build

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static("build"));
 
 morgan.token("body", function (req, res) {
 	return JSON.stringify(req.body);
@@ -27,8 +28,6 @@ app.use(
 		].join(" ");
 	})
 );
-
-// app.use(morgan("combined"));
 
 let persons = [
 	{
